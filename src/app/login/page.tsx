63576b60cc182a0ca9e714fe4f2379ab96b71f1e
Ahmad Faaiz Al-auza'i <afaaiz2@gmail.com>
2025-07-27 19:33:09 +0700

    fix: build error

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,15 @@
 import { LoginContainer } from "@/containers/login";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Login",
 };
 
 export default function Login() {
-  return <LoginContainer />;
+  return (
+    <Suspense>
+      <LoginContainer />
+    </Suspense>
+  );
 }
